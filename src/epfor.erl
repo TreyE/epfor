7d4ed4ae520e905a60fed0936463a6cf66c4c6a9
Trey Evans <lewis.r.evans@gmail.com>
2015-07-13 17:35:46 -0400

    Updating method call.

--- src/epfor.erl
+++ src/epfor.erl
@@ -2,7 +2,7 @@
 
 -export([link_packet_port/2, link_packet_port/3, call_packet_port/2, link_packet_port_pid/2]).
 
-link_packet_port_pid(CmdSpec, Options) -> epfor_packet_port:start_link(CmdSpec, OtherArgs).
+link_packet_port_pid(CmdSpec, OtherArgs) -> epfor_packet_port:start_link(CmdSpec, OtherArgs).
 
 link_packet_port(NameSpec, CmdSpec) -> link_packet_port(NameSpec, CmdSpec, []).
 link_packet_port(NameSpec, CmdSpec, OtherArgs) -> epfor_packet_port:start_link(NameSpec, CmdSpec, OtherArgs).
